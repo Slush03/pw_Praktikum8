ca736ec47341bba69f90ee2203846ac555677249
Slush03 <taufiq.ttgi@gmail.com>
2024-05-30 17:44:01 +0700

    vue

---
+++ praktikum8/src/components/aritclehome.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <img src="../assets/hotwheel-logo.png" alt="" style="width: 100px;">
+        <h1>ini adalah komponen baru</h1>
+        <p>ini adalah article</p>
+        <p>ini adalah article</p>
+        <p>ini adalah article</p>
+        <p>ini adalah article</p>
+    </div>
+</template>
---
+++ praktikum8/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+import articlehome from '../components/aritclehome.vue'
+</script>
+
+<template>
+  <main>
+    <articlehome></articlehome>
+  </main>
+</template>
